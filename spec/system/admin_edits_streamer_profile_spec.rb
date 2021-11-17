b7a39122fe4087d0d6746b83fb6503253cb03bc2
paulo-castellan <paulo.castellan@gmail.com>
2021-11-17 09:11:16 -0300

    adds avatar test to edit_streamer

--- spec/system/admin_edits_streamer_profile_spec.rb
+++ spec/system/admin_edits_streamer_profile_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Administrator edit streamer profile' do
   context 'successfully' do
-    it 'changes streamer name' do
+    it 'changes streamer profile' do
       admin = create(:user, :admin)
       create(:streamer, name: 'Fulano')
 
@@ -13,31 +13,14 @@ describe 'Administrator edit streamer profile' do
       click_link 'Editar Streamer'
       within 'form' do
         fill_in 'Nome', with: 'Sicrano'
+        attach_file('streamer[avatar]', 'spec/fixtures/files/avatar_placeholder_edition.png')
         click_button 'Atualizar Streamer'
       end
 
       expect(current_path).to eq streamer_path(Streamer.last)
       expect(page).to have_content('Cadastro atualizado com sucesso!')
       expect(page).to have_content('Nome: Sicrano')
-    end
-
-    it 'changes streamer avatar' do
-      admin = create(:user, :admin)
-      create(:streamer, name: 'Fulano')
-
-      login_as admin, scope: :user
-      visit root_path
-      click_link 'Streamers'
-      click_link 'Fulano'
-      click_link 'Editar Streamer'
-      within 'form' do
-        attach_file('streamer[avatar]', 'spec/fixtures/files/avatar_placeholder.png')
-        click_button 'Atualizar Streamer'
-      end
-
-      expect(current_path).to eq streamer_path(Streamer.last)
-      expect(page).to have_content('Cadastro atualizado com sucesso!')
-      expect(page).to have_content('Nome: Sicrano')
+      expect(page).to have_css("img[src*='avatar_placeholder_edition.png']")
     end
   end
 end
