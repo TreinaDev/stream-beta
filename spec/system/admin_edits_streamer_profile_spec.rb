a005ca9fe51e943173006342b99969c512e775d0
paulo-castellan <paulo.castellan@gmail.com>
2021-11-17 16:23:01 -0300

    removes useless file, tests for edit streamer

--- spec/system/admin_edits_streamer_profile_spec.rb
+++ spec/system/admin_edits_streamer_profile_spec.rb
@@ -14,6 +14,10 @@ describe 'Administrator edit streamer profile' do
       within 'form' do
         fill_in 'Nome', with: 'Sicrano'
         attach_file('streamer[avatar]', 'spec/fixtures/files/avatar_placeholder_edition.png')
+        fill_in 'Facebook', with: 'www.facebook.com/sicrano'
+        fill_in 'Canal no Youtube', with: 'www.youtube.com/sicrano'
+        fill_in 'Instagram', with: 'www.instagram.com/sicrano'
+        fill_in 'Twitter', with: 'www.twitter.com/sicrano'
         click_button 'Atualizar Streamer'
       end
 
@@ -21,10 +25,25 @@ describe 'Administrator edit streamer profile' do
       expect(page).to have_content('Cadastro atualizado com sucesso!')
       expect(page).to have_content('Nome: Sicrano')
       expect(page).to have_css("img[src*='avatar_placeholder_edition.png']")
+      expect(page).to have_content('Facebook: www.facebook.com/sicrano')
+      expect(page).to have_content('Youtube: www.youtube.com/sicrano')
+      expect(page).to have_content('Instagram: www.instagram.com/sicrano')
+      expect(page).to have_content('Twitter: www.twitter.com/sicrano')
     end
   end
 
   context 'fails' do
+    it 'due to not be logged in' do
+      admin = create(:user, :admin)
+      create(:streamer, name: 'Fulano')
+
+      visit root_path
+      click_link 'Streamers'
+      click_link 'Fulano'
+
+      expect(page).not_to have_link 'Editar Streamer'
+    end
+
     it 'due to empty fields' do
       admin = create(:user, :admin)
       create(:streamer, name: 'Fulano')
