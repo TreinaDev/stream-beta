212bbf0f4ec7ad76f1e3d01600705266b29cfc85
Yan Donadi <yan.donadi@gmail.com>
2021-11-16 23:01:53 -0300

    Adicionadas validações pra Video e implementado método para consultar Vimeo

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,18 @@
 class Video < ApplicationRecord
+  validates :title, :duration, :video_url, :maturity_rating, presence: true
+  validates :duration, format: { with: /\d{2}:[0-5]\d:[0-5]\d/, message: 'não está formatada corretamente' }
+  validates :video_url,
+            format: {
+              with: %r{(?:http|https)?(?:://)?(?:player\.)?vimeo\.com/(?:.*/)?\d{9}},
+              message: 'não está formatada corretamente'
+            }
+
+  def obtain_video_info_from_vimeo
+    source = "https://vimeo.com/api/oembed.json?url=#{video_url}"
+    response = Net::HTTP.get_response(URI.parse(source))
+
+    return false unless response.message == 'OK'
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
