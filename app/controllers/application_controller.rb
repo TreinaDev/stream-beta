31679222a1a10b18fa467e304bfa09f0281cd061
Yan Donadi <yan.donadi@gmail.com>
2021-11-17 03:30:58 -0300

    Isolado método de consulta de valor atual e validação de datas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success
-  
+
   def after_sign_in_path_for(_resource)
     if current_user.admin?
       admin_home_index_path
