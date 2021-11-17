2975714297bfc3b6bbfc71862a9766a39bbdc8bd
paulo-castellan <paulo.castellan@gmail.com>
2021-11-17 09:19:06 -0300

    adds test for edit streamer with empty field

--- spec/system/admin_edits_streamer_profile_spec.rb
+++ spec/system/admin_edits_streamer_profile_spec.rb
@@ -23,4 +23,24 @@ describe 'Administrator edit streamer profile' do
       expect(page).to have_css("img[src*='avatar_placeholder_edition.png']")
     end
   end
+
+  context 'fails' do
+    it 'due to empty fields' do
+      admin = create(:user, :admin)
+      create(:streamer, name: 'Fulano')
+
+      login_as admin, scope: :user
+      visit root_path
+      click_link 'Streamers'
+      click_link 'Fulano'
+      click_link 'Editar Streamer'
+      within 'form' do
+        fill_in 'Nome', with: ''
+        click_button 'Atualizar Streamer'
+      end
+
+      expect(current_path).to eq streamer_path(Streamer.last)
+      expect(page).to have_content('Nome não pode ficar em branco')
+    end
+  end
 end
