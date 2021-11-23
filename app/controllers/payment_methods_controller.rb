f8548340ae8481ada365ba9333cdbe183a743d1e
Mateus Campos <mateus.camphos2000@gmail.com>
2021-11-23 16:50:37 -0300

    Restringindo rotas específicas de user
    Co-authored-by: Yan Donadi <yan.donadi@gmail.com>

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -1,7 +1,7 @@
 class PaymentMethodsController < ApplicationController
   before_action :authenticate_user!
   before_action :user_must_fill_profile
-  before_action :deny_admin_access, only: %i[new create]
+  before_action :deny_admin_access, only: %i[new create show]
 
   def new
     @payment_method = PaymentMethod.new
