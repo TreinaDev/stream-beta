f43544ce1d981bce8d040d1995e93eca48a2d9e4
paulo-castellan <paulo.castellan@gmail.com>
2021-11-24 18:14:09 -0300

    testa associacao de playlist com streamers

--- spec/system/admin_creates_playlist_spec.rb
+++ spec/system/admin_creates_playlist_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'Administrator creates playlist' do
   it 'successfully without streamer' do
     admin = create(:user, :admin)
+    create(:streamer, name: 'Fulaninho')
 
     login_as admin, scope: :user
     visit root_path
@@ -19,11 +20,13 @@ describe 'Administrator creates playlist' do
     expect(page).to have_css('div', text: 'Playlist criada com sucesso!')
     expect(page).to have_content('Título: As melhores jogadas')
     expect(page).to have_content('Descrição: Playlist com as melhores jogadas dos streamers participantes')
+    expect(page).not_to have_content('Streamer: Fulaninho')
   end
 
-  it 'successfully with streamer' do
+  it 'successfully with one out of two streamers' do
     admin = create(:user, :admin)
     create(:streamer, name: 'Fulaninho')
+    create(:streamer, name: 'Sicraninho')
 
     login_as admin, scope: :user
     visit root_path
@@ -40,8 +43,37 @@ describe 'Administrator creates playlist' do
     expect(current_path).to eq(playlist_path(Playlist.last))
     expect(page).to have_css('div', text: 'Playlist criada com sucesso!')
     expect(page).to have_content('Título: As melhores jogadas')
-    expect(page).to have_content('Streamer: Fulaninho')
     expect(page).to have_content('Descrição: Playlist com as melhores jogadas dos streamers participantes')
+    expect(page).to have_content('Streamer: Fulaninho')
+    expect(page).not_to have_content('Streamer: Sicraninho')
+  end
+
+  it 'successfully with two out of three streamers' do
+    admin = create(:user, :admin)
+    create(:streamer, name: 'Fulaninho')
+    create(:streamer, name: 'Sicraninho')
+    create(:streamer, name: 'Beltraninho')
+
+    login_as admin, scope: :user
+    visit root_path
+    click_link 'Playlists'
+    click_link 'Nova Playlist'
+    within 'form' do
+      fill_in 'Título', with: 'As melhores jogadas'
+      fill_in 'Descrição', with: 'Playlist com as melhores jogadas dos streamers participantes'
+      attach_file('playlist[playlist_cover]', 'spec/fixtures/files/avatar_placeholder.png')
+      check 'Fulaninho'
+      check 'Sicraninho'
+      click_button 'Criar Playlist'
+    end
+
+    expect(current_path).to eq(playlist_path(Playlist.last))
+    expect(page).to have_css('div', text: 'Playlist criada com sucesso!')
+    expect(page).to have_content('Título: As melhores jogadas')
+    expect(page).to have_content('Descrição: Playlist com as melhores jogadas dos streamers participantes')
+    expect(page).to have_content('Streamer: Fulaninho')
+    expect(page).to have_content('Streamer: Sicraninho')
+    expect(page).not_to have_content('Streamer: Beltraninho')
   end
 
   it 'but fails due to missing fields' do
