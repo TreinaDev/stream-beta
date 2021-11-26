7bbf5041581cc9f464e1f04e6bc6a74129e3a179
daiaventureira <daiane.souzas@icloud.com>
2021-11-25 20:09:53 -0500

    Finaliza testes:

--- spec/system/admin/admin_removes_video_from_playlist_spec.rb
+++ spec/system/admin/admin_removes_video_from_playlist_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'Admin removes video from playlist' do
   it 'successfully' do
     admin = create(:user, :admin)
-    create(:video, title: 'Outro video', video_url: 'https://vimeo.com/987654321')
-    create(:video, title: 'Video', video_url: 'https://vimeo.com/123456789')
-    playlist = create(:playlist, title: 'Playlist de jogos', videos: [video])
+    outro_video = create(:video, title: 'Outro video', video_url: 'https://vimeo.com/987654321')
+    video = create(:video, title: 'Videozinho', video_url: 'https://vimeo.com/123456789')
+    playlist = create(:playlist, title: 'Playlist de jogos', video_ids: [video.id, outro_video.id])
 
     login_as admin, scope: :user
     visit root_path
@@ -14,14 +14,14 @@ describe 'Admin removes video from playlist' do
     click_link 'Playlist de jogos'
     click_link 'Editar playlist'
     within 'form' do
-      uncheck 'Video'
+      uncheck 'Videozinho'
       click_button 'Atualizar Playlist'
     end
 
     expect(current_path).to eq(playlist_path(playlist.id))
     expect(page).to have_content('Título: Playlist de jogos')
     expect(page).to have_content('Outro video')
-    expect(page).not_to have_content('Video')
+    expect(page).not_to have_content('Videozinho')
     expect(page).not_to have_link('https://vimeo.com/123456789')
   end
 end
