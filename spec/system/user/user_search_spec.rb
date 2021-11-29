64638fa4bb6282850b4392a124700740bf46be71
Hugo Araujo <hugo17@protonmail.com>
2021-11-29 17:47:16 -0300

    Add teste para pesquisa vazia e colocar localizar pagina search

--- spec/system/user/user_search_spec.rb
+++ spec/system/user/user_search_spec.rb
@@ -2,22 +2,38 @@ require 'rails_helper'
 
 describe 'User search' do
   describe 'streamers' do
-    it 'successfully' do
-      user = create(:user)
-      create(:user_profile, user: user)
-      create(:streamer, name: 'joaozinho')
-      create(:streamer, name: 'maria')
+    context 'successfully' do
+      it 'with 2 streamers' do
+        user = create(:user)
+        create(:user_profile, user: user)
+        create(:streamer, name: 'joaozinho')
+        create(:streamer, name: 'maria')
 
-      login_as user, scope: :user
-      visit root_path
-      click_on 'Streamers'
-      fill_in 'Busca:', with: 'maria'
-      click_button 'Pesquisar'
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Streamers'
+        fill_in 'Busca:', with: 'maria'
+        click_button 'Pesquisar'
 
-      expect(current_path).to eq(search_streamers_path)
-      expect(page).to have_content('maria')
-      expect(page).to have_content('Pesquisa de Streamers')
-      expect(page).not_to have_content('joaozinho')
+        expect(current_path).to eq(search_streamers_path)
+        expect(page).to have_content('maria')
+        expect(page).to have_content('Pesquisa de Streamers')
+        expect(page).not_to have_content('joaozinho')
+      end
+      it 'with nothing' do
+        user = create(:user)
+        create(:user_profile, user: user)
+
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Streamers'
+        fill_in 'Busca:', with: 'maria'
+        click_button 'Pesquisar'
+
+        expect(current_path).to eq(search_streamers_path)
+        expect(page).to have_content('Pesquisa de Streamers')
+        expect(page).to have_content('Nenhum streamer encontrado')
+      end
     end
   end
 end
