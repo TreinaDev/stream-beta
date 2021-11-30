42cc3c177d716160b6437fe8a08444f33677c4e3
Hugo Araujo <hugo17@protonmail.com>
2021-11-30 20:14:42 -0300

    Add relatório de playlists

--- app/controllers/admin/report_controller.rb
+++ app/controllers/admin/report_controller.rb
@@ -4,6 +4,9 @@ module Admin
       streamers = Streamer.all
       @streamers = streamers.zip streamers.map(&:users).map(&:count)
       @streamers.sort_by!(&:last).reverse!
+      playlists = Playlist.all
+      @playlists = playlists.zip playlists.map(&:users).map(&:count)
+      @playlists.sort_by!(&:last).reverse!
     end
   end
 end
--- spec/system/admin/admin_see_report_spec.rb
+++ spec/system/admin/admin_see_report_spec.rb
@@ -17,5 +17,20 @@ describe 'Administrator see report' do
       expect(page).to have_content('João 8')
       expect(page).to have_content('Maria 0')
     end
+    it 'number subscriptions playlist' do
+      admin = create(:user, :admin)
+      playlist = create(:playlist, title: 'Playlist Interessante')
+      create(:streamer, name: 'Playlist vazia')
+      subscription = create(:subscription_plan, playlists: [playlist])
+      8.times.each { create(:user_subscription_plan, subscription_plan: subscription) }
+      login_as admin, scope: :user
+      visit root_path
+      click_on 'Relatório'
+
+      expect(current_path).to eq(admin_report_path)
+      expect(page).to have_content('Relatório')
+      expect(page).to have_content('Playlist Interessante 8')
+      expect(page).to have_content('Playlist vazia 0')
+    end
   end
 end
