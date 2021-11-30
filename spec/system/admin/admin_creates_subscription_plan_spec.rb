2c1ec4d024040d73525714d04f9d96e8e06f37dc
Yan Donadi <yan.donadi@gmail.com>
2021-11-30 18:21:15 -0300

    Criado fluxo de verificação de formas de pagamento disponíveis por API e adequado testes

--- spec/system/admin/admin_creates_subscription_plan_spec.rb
+++ spec/system/admin/admin_creates_subscription_plan_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 describe 'Administrator creates new subscription plan' do
   it 'successfully' do
     admin = create(:user, :admin)
-    allow_any_instance_of(SubscriptionPlan).to receive(:generate_new_token).and_return('abcABC1234')
+    request = { title: 'Plano Padrão', value: '50.0' }.to_json
+    response = { subscription_plan_token: '0YDlXnLGnS' }.to_json
+    fake_response = instance_double(Faraday::Response, status: 201, body: response)
+    header = { 'Content-Type' => 'application/json',
+               'company_token' => Rails.configuration.api_pagapaga[:company_token] }
+    allow(Faraday).to receive(:post).with('http://localhost:4000/api/v1/subscription_plans/', request, header)
+                                    .and_return(fake_response)
 
     login_as admin, scope: :user
     visit root_path
@@ -16,7 +22,9 @@ describe 'Administrator creates new subscription plan' do
       click_button 'Criar Plano'
     end
 
-    expect(current_path).to eq(subscription_plan_path(SubscriptionPlan.last))
+    subscription_plan = SubscriptionPlan.last
+    expect(current_path).to eq(subscription_plan_path(subscription_plan))
+    expect(subscription_plan.token).to eq('0YDlXnLGnS')
     expect(page).to have_css('div', text: 'Plano criado com sucesso!')
     expect(page).to have_content('Plano Padrão')
     expect(page).to have_content('Esse plano é o plano padrão')
@@ -25,7 +33,13 @@ describe 'Administrator creates new subscription plan' do
 
   it 'fails due to empty fields' do
     admin = create(:user, :admin)
-    allow_any_instance_of(SubscriptionPlan).to receive(:generate_new_token).and_return('abcABC1234')
+    request = { title: '', value: nil }.to_json
+    response = { subscription_plan_token: '' }.to_json
+    fake_response = instance_double(Faraday::Response, status: 201, body: response)
+    header = { 'Content-Type' => 'application/json',
+               'company_token' => Rails.configuration.api_pagapaga[:company_token] }
+    allow(Faraday).to receive(:post).with('http://localhost:4000/api/v1/subscription_plans/', request, header)
+                                    .and_return(fake_response)
 
     login_as admin, scope: :user
     visit root_path
