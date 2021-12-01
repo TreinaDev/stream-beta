5b9db3c57c03b56d0c2406fb5c5fc24f60187489
Hugo Araujo <hugo17@protonmail.com>
2021-11-30 21:30:55 -0300

    Add relatório de videos

--- app/controllers/admin/report_controller.rb
+++ app/controllers/admin/report_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class ReportController < ApplicationController
+    before_action :authenticate_admin!, only: :report
     def report
       streamers = Streamer.all
       @streamers = streamers.zip streamers.map(&:users).map(&:count)
@@ -7,6 +8,9 @@ module Admin
       playlists = Playlist.all
       @playlists = playlists.zip playlists.map(&:users).map(&:count)
       @playlists.sort_by!(&:last).reverse!
+      videos = Video.select(&:allow_purchase?)
+      @videos = videos.zip videos.map(&:users).map(&:count)
+      @videos.sort_by!(&:last).reverse!
     end
   end
 end
--- spec/system/admin/admin_see_report_spec.rb
+++ spec/system/admin/admin_see_report_spec.rb
@@ -32,5 +32,31 @@ describe 'Administrator see report' do
       expect(page).to have_content('Playlist Interessante 8')
       expect(page).to have_content('Playlist vazia 0')
     end
+    it 'number subscriptions videos' do
+      admin = create(:user, :admin)
+      video = create(:video, :allow_purchase, title: 'Vídeo Muito Popular')
+      create(:video, :allow_purchase, title: 'video q ninguem compra')
+      8.times.each { create(:user_video, video: video) }
+      login_as admin, scope: :user
+      visit root_path
+      click_on 'Relatório'
+
+      expect(current_path).to eq(admin_report_path)
+      expect(page).to have_content('Relatório')
+      expect(page).to have_content('Vídeo Muito Popular 8')
+      expect(page).to have_content('video q ninguem compra')
+    end
+    it 'nothing' do
+      admin = create(:user, :admin)
+
+      login_as admin, scope: :user
+      visit root_path
+      click_on 'Relatório'
+
+      expect(current_path).to eq(admin_report_path)
+      expect(page).to have_content('Nenhum streamer com assinante')
+      expect(page).to have_content('Nenhum vídeo com assinante')
+      expect(page).to have_content('Nenhuma playlist com assinante')
+    end
   end
 end
