bfc3544ba358b30937198a6358be36cb37f91fc4
daiaventureira <daiane.souzas@icloud.com>
2021-12-02 11:59:34 -0500

    Fixa testes e muda layout

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :video do
     title { FFaker::Game.title }
     duration { FFaker::String.from_regexp(/0\d:[0-5]\d:[0-5]\d/) }
-    video_url { "https://vimeo.com/#{FFaker::String.from_regexp(/\d{9}/)}" }
+    video_url { FFaker::String.from_regexp(/\d{9}/) }
     maturity_rating { %w[L 10 12 14 16 18].sample }
 
     streamer
